6503607b9bef5d59ee542d795403d783b2a29983
nbhaohao <nbhaohao@163.com>
2023-02-19 11:22:48 +0800

    Started simple mvc

--- src/public/index.php
+++ src/public/index.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\PaymentGateway\Paddle\Transaction;
-
-$paddleTransaction = new Transaction();
-
-$id = new \Ramsey\Uuid\UuidFactory();
-
-echo $id->uuid4();
-var_dump($paddleTransaction);
+echo '<pre>';
+print_r($_SERVER);
+echo '</pre>';
