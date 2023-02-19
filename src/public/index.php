9f458b3b1fcdc205263128832ca19ac2b1af4e31
nbhaohao <nbhaohao@163.com>
2023-02-19 11:44:55 +0800

    Simple router

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
+$router = new App\Router();
+
+$router
+    ->register('/', [\App\Classes\Home::class, 'index'])
+    ->register('/invoices', [\App\Classes\Invoice::class, 'index'])
+    ->register('/invoices/create', [\App\Classes\Invoice::class, 'create']);
+
+echo $router->resolve($_SERVER['REQUEST_URI']);
