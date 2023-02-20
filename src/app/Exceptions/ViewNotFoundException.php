ad25af5b005fba0aaae68f0f9dad8d0cf11ad5d9
nbhaohao <nbhaohao@163.com>
2023-02-20 20:16:12 +0800

    Finished 404 page

--- src/app/Exceptions/ViewNotFoundException.php
+++ src/app/Exceptions/ViewNotFoundException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Exceptions;
 
 class ViewNotFoundException extends \Exception
 {
--- src/public/index.php
+++ src/public/index.php
@@ -7,13 +7,19 @@ require_once __DIR__ . '/../vendor/autoload.php';
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEW_PATH', __DIR__ . '/../views');
 
-$router = new App\Router();
+try {
 
-$router
-    ->get('/', [\App\Controllers\HomeController::class, 'index'])
-    ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
-    ->get('/invoices', [\App\Controllers\InvoiceController::class, 'index'])
-    ->get('/invoices/create', [\App\Controllers\InvoiceController::class, 'create'])
-    ->post('/invoices/create', [\App\Controllers\InvoiceController::class, 'store']);
+    $router = new App\Router();
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+    $router
+        ->get('/', [\App\Controllers\HomeController::class, 'index'])
+        ->post('/upload', [\App\Controllers\HomeController::class, 'upload'])
+        ->get('/invoices', [\App\Controllers\InvoiceController::class, 'index'])
+        ->get('/invoices/create', [\App\Controllers\InvoiceController::class, 'create'])
+        ->post('/invoices/create', [\App\Controllers\InvoiceController::class, 'store']);
+
+    echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+} catch (\App\Exceptions\RouteNotFoundException $e) {
+    http_response_code(404);
+    echo \App\View::make('error/404');
+}
